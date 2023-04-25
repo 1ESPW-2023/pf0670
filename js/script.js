0ceea320a70f2383f59734116292e4443cdfe4b6
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-04-25 20:34:33 -0300

    Rotação de banner com setTimeOut Math.

--- js/script.js
+++ js/script.js
@@ -41,7 +41,6 @@
 // });
 let tmp = 0;
 
-
 function mudaCor() {
     let r = Math.ceil(Math.random()*255);
     let g = Math.ceil(Math.random()*255);
@@ -56,5 +55,40 @@ function mudaCor() {
 
 }
 
+function alteraBanner1() {
+    
+    // let nr = Math.ceil(Math.random() *3);
+    const img1 = document.querySelector(".l-d > img");
+    const img2 = document.querySelector(".l-e > img");
+    img1.src = "./img/banner-lateral-1.png";
+    img2.src = "./img/banner-lateral-2.png";
 
+    setTimeout(alteraBanner2, 1000);
+
+}
  
+function alteraBanner2() {
+    
+    // let nr = Math.ceil(Math.random() *3);
+    const img1 = document.querySelector(".l-d > img");
+    const img2 = document.querySelector(".l-e > img");
+    img1.src = "./img/banner-lateral-2.png";
+    img2.src = "./img/banner-lateral-1.png";
+
+    setTimeout(alteraBanner3, 1000);
+
+}
+
+function alteraBanner3() {
+    
+    // let nr = Math.ceil(Math.random() *3);
+    const img1 = document.querySelector(".l-d > img");
+    const img2 = document.querySelector(".l-e > img");
+    img1.src = "./img/banner-lateral-1.png";
+    img2.src = "./img/banner-lateral-2.png";
+
+    setTimeout(alteraBanner1, 1000);
+
+}
+
+alteraBanner1();
