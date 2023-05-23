e5a73a7085d8a04c6789a7ab30393d360dbf7b07
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-05-23 20:39:54 -0300

    ValidaForm autenticação c LStorage+token/Redirect.

--- js/home.js
+++ js/home.js
@@ -1,5 +1,5 @@
 
-if(JSON.parse(localStorage.getItem("usuario-validado")) != null){
+if(localStorage.getItem("usuario-token")){
     //Exercício
     //Analisando o objeto que chega na página home através do localStorage faça com que somente se o objeto possuir conteúdo o usuário possa permanecer na página.
     //Utilize os mesmos conceitos mostrandos agora de redirecionamento e validação de objetos.
@@ -7,14 +7,30 @@ if(JSON.parse(localStorage.getItem("usuario-validado")) != null){
     //Recepcionando o objeto que logou no localStorage e imprimindo uma de suas propriedades.
     const spanUser = document.querySelector("#user");
     let usuario = JSON.parse(localStorage.getItem("usuario-validado"))
-    spanUser.innerHTML = usuario.nomeCompleto;
+    
+    //Exercício
+    //Recebendo os dados do objeto e construindo interface.
+    //Criar elementos HTML que recebam os atributos do objeto USUARIO-VALIDADO.
+    //Crie um CARD onde seja apresentado os seguintes dados do usuário:
+    // - nomeCompleto
+    // - nomeUsuario
+    // - avatarUsuario
+
+    const imgAvatar = document.querySelector("#img-avatar");
+    imgAvatar.src = usuario.avatarUsuario;
+    imgAvatar.alt = usuario.nomeCompleto;
+    imgAvatar.title = usuario.nomeUsuario;
+
+    const legUsuario = document.querySelector("#legenda");
+    legUsuario.textContent = usuario.nomeCompleto;
 
     const botaoLogout = document.querySelector("#btnLogout");
     botaoLogout.addEventListener("click", ()=>{
         localStorage.removeItem("usuario-validado");
-        window.location.href = "../login.html";    
+        localStorage.removeItem("usuario-token");
+        window.location.href = "../signin.html";
     });
 
 }else{
-    window.location.href = "../login.html";
+    window.location.href = "../signin.html";
 }
--- js/signin.js
+++ js/signin.js
@@ -1,65 +1,65 @@
 "use strict";
 
-
-
 //CRIANDO OBJETOS
 //CRIAR UMA LISTA DE OBJETOS
 let listaDeUsuarios = [
     {
         nomeCompleto : "José da Silva",
         nomeUsuario : "jose",
-        senhaUsuario: "123456"
+        senhaUsuario: "123456",
+        avatarUsuario : "./img/profile-42914_640.png"
     }
     ,
     {
         nomeCompleto : "Paulo da Silva",
         nomeUsuario : "paulo",
-        senhaUsuario: "123456"
+        senhaUsuario: "123456",
+        avatarUsuario : "./img/profile-42914_640.png"
     }
     ,
     {
         nomeCompleto : "Maria da Silva",
         nomeUsuario : "maria",
-        senhaUsuario: "123456"
+        senhaUsuario: "123456",
+        avatarUsuario : "./img/profile-42914_640.png"
     }
     ,
     {
         nomeCompleto : "João da Silva",
         nomeUsuario : "joao",
-        senhaUsuario: "123456"
+        senhaUsuario: "123456",
+        avatarUsuario : "./img/profile-42914_640.png"
     }
 ];
 
-//Adicionando a lista de OBJETOS no localStorage
+// //Adicionando a lista de OBJETOS no localStorage
 localStorage.setItem("listaUser", JSON.stringify(listaDeUsuarios));
 
 addEventListener("click", (evt)=>{
     if(evt.target.id == "btnSubmit"){
-
-        //Entrada do usuário
-        let usuarioValidado = {
-            usuarioInput : document.querySelector("#usuario").value,
-            senhaInput : document.querySelector("#senha").value
-        }
         const msgError = document.querySelector("#msgError");
 
-        try{
+        //Entrada do usuário
+        let usuarioLogado = {
+            usuarioInputLogado : document.querySelector("#usuario").value,
+            senhaInputLogado : document.querySelector("#senha").value
+        }
 
-            let listaUser = JSON.parse(localStorage.getItem("listaUser"));
+        let usuarioValidado = {};
 
-            listaUser.forEach((usuario)=>{
+        let listaUser = JSON.parse(localStorage.getItem("listaUser"));
 
-                if(usuarioValidado.usuarioInput == usuario.nomeUsuario && usuarioValidado.senhaInput == usuario.senhaUsuario){
+            for (let x = 0; x < listaUser.length; x++) {
+                
+                if(usuarioLogado.usuarioInputLogado == listaUser[x].nomeUsuario && usuarioLogado.senhaInputLogado == listaUser[x].senhaUsuario){
                     //Adicionando nova propriedade no objeto.
-                    usuarioValidado["nomeCompleto"] = usuario.nomeCompleto;
-                    throw "USUÁRIO VALIDADO!";
+                    usuarioValidado = listaUser[x];
+                    break;
                 }
-            });
+            }
 
-            throw "SENHA OU NOME DE USUÁRIO INVÁLIDO!";
-        }catch(msg){
 
-            if(msg == "USUÁRIO VALIDADO!"){
+            if(usuarioValidado.nomeUsuario != null){
                 
                 //Adicionando o objeto usuarioValidado no localStorage
                 localStorage.setItem("usuario-validado", JSON.stringify(usuarioValidado))
@@ -67,18 +67,20 @@ addEventListener("click", (evt)=>{
                 msgError.setAttribute("style","color:#00ff00;display:block;")
                 msgError.innerHTML = "<strong>Usuário validado!</strong>"
 
+                //Criar token para autenticação do usuário
+                const token = Math.random().toString(16).substring(2)+Math.random().toString(16).substring(2);
+                //Adicionando o token no localStorage
+                localStorage.setItem("usuario-token", token);
+
                 //Criando o sistema de redirect
                 setTimeout(function () {
                     window.location.href = "../home.html";
                 }, 3000 );
-
-            }else{
+            }
+            else{
                 msgError.setAttribute("style","color:#ff0000;display:block;");
                 msgError.innerHTML = "<strong>Senha ou nome de Usuário inválidos!</strong>";
             }
-        }
-           
-
-
     }
+
 });
